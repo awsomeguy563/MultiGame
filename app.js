dbfae65d66d116d5264d6cc937b7dbf8d6e115df
Hrishit Leen <hrishitleen@Hrishits-MacBook-Pro.local>
2020-04-13 15:16:58 +0400

    heroku deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require("express")
 let app = express()
 let serv = require("http").createServer(app)
-port = 3000
+port = process.env.PORT || 3000
 
 
 app.get("/", function(req, res){
